1efcbbbbefe9a1dccb704d0537ad6aed85881178
igormivanov <igormivanovx@gmail.com>
2023-02-11 18:35:00 -0300

    As file

---
+++ modulo1/as.sql
@@ -0,0 +1,5 @@
+SELECT nome, idade, matricula FROM aluno;
+
+SELECT nome as "Nome do Aluno", idade, matricula FROM aluno;
+
+// as(alias) servem para renomear, apelidar um campo(coluna) temporariamente
