59450717930926f8db5f61bb32dbc4de13f38397
igormivanov <igormivanovx@gmail.com>
2023-02-11 17:11:20 -0300

    create table atualizado

--- modulo1/create_table.sql
+++ modulo1/create_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE aluno (
-	id SERIAL, 				// Irá incrementar uma sequencia automatica ao id
+	id SERIAL, 			// Irá incrementar uma sequencia automatica ao id
 	nome VARCHAR(255), 		// Tipo String, podendo ir até 255 caracteres
 	cpf char(11),			// Para poucos caracteres 
 	observacao TEXT,		// Texto sem limitação de tamanho
 	idade INTEGER,			// Inteiro 
 	ativo BOOLEAN,			// True, false, null
-	data_nascimento DATE,	// Data -> 2023-02-08
+	data_nascimento DATE,	        // Data -> 2023-02-08
 	hora_aula TIME,			// Horario -> 17:14:00
 	matricula TIMESTAMP		// Data e hora -> 17:14:00 2023-02-08
 );
