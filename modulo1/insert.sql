7593a53e63bbbee6c8ea500e0ae73b9ba7b66cb8
igormivanov <igormivanovx@gmail.com>
2023-02-11 17:35:27 -0300

    insert file

---
+++ modulo1/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO aluno (nome, cpf, observacao, idade, ativo, data_nascimento, hora_aula, matricula) 
+	VALUES ('igor', '12345678901', 'teste', 22, true, '2023-02-08', '17:14:00', '2023-02-08 17:14:00');
+	
+	
