d104d4357388640b6c4b86a439d473a98747e621
igormivanov <igormivanovx@gmail.com>
2023-02-11 18:14:38 -0300

    Update file

---
+++ modulo1/update.sql
@@ -0,0 +1,10 @@
+UPDATE aluno
+	SET nome = 'Ana',
+	cpf = '10987654321',
+	observacao = 'ola',
+	idade = 24,
+	ativo = false,
+	data_nascimento = '2023-02-11',
+	hora_aula = '18:11:00',
+	matricula = '2023-02-11 18:11:00'
+	WHERE ID = 1;
